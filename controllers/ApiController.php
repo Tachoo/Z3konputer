fd7235af4902f2fdd956f5a9d60089e27b499092
Sergianh <137209397+Sergianh@users.noreply.github.com>
2023-09-16 23:37:24 -0600

    try to render view

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -1,12 +1,15 @@
 <?php
 namespace Controllers;
 use Model\Productos;
+use MVC\Router;
 class APIController
 { 
-    public static function productos()
+    public static function productos(Router $_routers)
     {
         $_productos=Productos::all();
         //debuguear($_productos);
         echo json_encode($_productos);
+
+        $_routers->render('single/single-product');
     }
 }
