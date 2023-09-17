06c4d50c4be7665e826a3d873c2af13fd90dc9c4
Sergianh <137209397+Sergianh@users.noreply.github.com>
2023-09-16 23:59:22 -0600

    aaa

--- Router.php
+++ Router.php
@@ -27,8 +27,9 @@ class Router
         // $rutas_protegidas = ['/admin', '/propiedades/crear', '/propiedades/actualizar', '/propiedades/eliminar', '/vendedores/crear', '/vendedores/actualizar', '/vendedores/eliminar'];
 
         // $auth = $_SESSION['login'] ?? null;
-         $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        //$currentUrl = $_SERVER['PATH_INFO'] ?? '/';
         //$currentUrl = strtok($_SERVER['REQUEST_URI'],'?') ?? '/';
+        $currentUrl = $_SERVER['REQUEST_URI'] ?? '/';
         $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === 'GET') {
